fcaf69790faff64ff57169893e2a8dbae4fa3374
Trinity Terry <trinityterry@Trinitys-MacBook-Air.local>
2017-09-11 19:41:05 -0500

    9/11/17 7:40p

---
+++ lab2-4.js
@@ -0,0 +1,25 @@
+//Trinity Terry
+//9/11/17
+
+//This program will demonstrate various ways to
+//use functions in JavaScript.
+
+//The main function
+function main ()
+{
+    //Declare program variables
+    var totalSales;
+    var countyTax;
+    var stateTax;
+    var totalTax;
+     //Call welcomeMessage()
+     welcomeMessage ();
+}
+
+//This function is to welcome people to my program
+function welcomeMessage ()
+{
+    alert("Welcome to my program using functions");
+}
+
+main ()
